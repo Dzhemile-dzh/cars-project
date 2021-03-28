02388f45a8ff5f3d476dd3be7ab46bcefd3413cc
Dzhemile-dzh <dzhemile_ahmet@abv.bg>
2021-03-28 11:51:50 +0300

    но

--- config.php
+++
@@ -1,8 +0,0 @@
- <?php
-$servername = "51.91.16.72";
-$username = "dev19";
-$password = "HkJ8Jzw&z#";
-$conn = new PDO("mysql:host=$servername;dbname=dev19", $username, $password);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
-
